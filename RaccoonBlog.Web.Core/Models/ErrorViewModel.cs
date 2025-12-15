13792b1c26ef668d5b13254c3731524da8d25827
VladShpilMan <vladislav.lanets@gmail.com>
2025-12-15 10:44:45 +0100

    HRINT-4444 - add .net 8

---
+++ RaccoonBlog.Web.Core/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace RaccoonBlog.Web.Core.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
